afd45fd1407218cce20deb2fbdc1204e3e91b01d
ordinarylive <ordinarylive@163.com>
2019-03-09 11:26:43 +0800

    添加了初始化脚本

---
+++ social/api.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+
+# Create your views here.
+from libs.http import render_json
+
+
+def rcmd_users(request):
+    #获取推荐列表
+
+    return render_json()
+
+
+
+
+def like(request):
+    return render_json()
+
+
+def superlike(request):
+    return render_json()
+
+
+def dislike(request):
+    return render_json()
+
+
+def rewind(request):
+    return render_json()
+
+def show_liked_me(request):
+    return render_json()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- social/models.py
+++ social/models.py
@@ -29,4 +29,3 @@ class Swiped(models.Model):
 
 
 
-    
